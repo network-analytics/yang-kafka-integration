2bbd68aabdddcf8cf532d77d882ea2566fa9bd02
Alex Huang Feng <alex.huang-feng@insa-lyon.fr>
2025-03-28 17:36:39 +0100

    Add YANG JSON serde

---
+++ yang-json-schema-serializer/src/main/java/fr/insa/kafka/formatter/yang/json/YangJsonSchemaMessageReader.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2025 INSA Lyon.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.insa.kafka.formatter.yang.json;
+
+import ch.swisscom.kafka.schemaregistry.yang.YangSchema;
+import ch.swisscom.kafka.schemaregistry.yang.YangSchemaProvider;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.insa.kafka.serializers.yang.json.AbstractKafkaYangJsonSchemaSerializer;
+import fr.insa.kafka.serializers.yang.json.KafkaYangJsonSchemaSerializerConfig;
+import io.confluent.kafka.formatter.SchemaMessageReader;
+import io.confluent.kafka.formatter.SchemaMessageSerializer;
+import io.confluent.kafka.schemaregistry.ParsedSchema;
+import io.confluent.kafka.schemaregistry.SchemaProvider;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.json.jackson.Jackson;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Map;
+import kafka.common.MessageReader;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Serializer;
+import org.everit.json.schema.ValidationException;
+
+public class YangJsonSchemaMessageReader extends SchemaMessageReader<JsonNode>
+    implements MessageReader {
+
+  private static final ObjectMapper objectMapper = Jackson.newObjectMapper();
+
+  /** Constructor needed by kafka console producer. */
+  public YangJsonSchemaMessageReader() {}
+
+  /** For testing only. */
+  YangJsonSchemaMessageReader(
+      String url,
+      YangSchema keySchema,
+      YangSchema valueSchema,
+      String topic,
+      boolean parseKey,
+      BufferedReader reader,
+      boolean normalizeSchema,
+      boolean autoRegister,
+      boolean useLatest) {
+    super(
+        url,
+        keySchema,
+        valueSchema,
+        topic,
+        parseKey,
+        reader,
+        normalizeSchema,
+        autoRegister,
+        useLatest);
+  }
+
+  @Override
+  protected SchemaMessageSerializer<JsonNode> createSerializer(Serializer keySerializer) {
+    return new YangJsonJsonSchemaSerializer(keySerializer);
+  }
+
+  @Override
+  protected JsonNode readFrom(String jsonString, ParsedSchema schema) {
+    try {
+      return objectMapper.readTree(jsonString);
+    } catch (IOException | ValidationException e) {
+      throw new SerializationException(
+          String.format("Error serializing yang-json %s", jsonString), e);
+    }
+  }
+
+  @Override
+  protected SchemaProvider getProvider() {
+    return new YangSchemaProvider();
+  }
+
+  static class YangJsonJsonSchemaSerializer extends AbstractKafkaYangJsonSchemaSerializer<JsonNode>
+      implements SchemaMessageSerializer<JsonNode> {
+
+    protected final Serializer keySerializer;
+
+    YangJsonJsonSchemaSerializer(Serializer keySerializer) {
+      this.keySerializer = keySerializer;
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+      if (!configs.containsKey(KafkaYangJsonSchemaSerializerConfig.YANG_JSON_FAIL_INVALID_SCHEMA)) {
+        ((Map<String, Object>) configs)
+            .put(KafkaYangJsonSchemaSerializerConfig.YANG_JSON_FAIL_INVALID_SCHEMA, "true");
+      }
+      configure(new KafkaYangJsonSchemaSerializerConfig(configs));
+    }
+
+    @Override
+    public Serializer getKeySerializer() {
+      return keySerializer;
+    }
+
+    @Override
+    public byte[] serializeKey(String topic, Headers headers, Object payload) {
+      return keySerializer.serialize(topic, headers, payload);
+    }
+
+    @Override
+    public byte[] serialize(
+        String subject,
+        String topic,
+        boolean isKey,
+        Headers headers,
+        JsonNode object,
+        ParsedSchema schema) {
+      return super.serializeImpl(subject, topic, headers, object, (YangSchema) schema);
+    }
+
+    @Override
+    public SchemaRegistryClient getSchemaRegistryClient() {
+      return schemaRegistry;
+    }
+  }
+}
---
+++ yang-json-schema-serializer/src/test/java/fr/insa/kafka/formatter/yang/json/KafkaYangJsonSchemaFormatterTest.java
@@ -0,0 +1,134 @@
+package fr.insa.kafka.formatter.yang.json;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import ch.swisscom.kafka.schemaregistry.yang.YangSchema;
+import ch.swisscom.kafka.schemaregistry.yang.YangSchemaProvider;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.insa.kafka.serializers.yang.json.AbstractKafkaYangJsonSchemaSerializer;
+import fr.insa.kafka.serializers.yang.json.KafkaYangJsonSchemaDeserializerConfig;
+import fr.insa.kafka.serializers.yang.json.KafkaYangJsonSchemaSerializerTest;
+import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.testutil.MockSchemaRegistry;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.util.Collections;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.header.internals.RecordHeaders;
+import org.apache.kafka.common.record.TimestampType;
+import org.dom4j.DocumentException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.yangcentral.yangkit.model.api.schema.YangSchemaContext;
+import org.yangcentral.yangkit.parser.YangParserException;
+import org.yangcentral.yangkit.parser.YangYinParser;
+
+public class KafkaYangJsonSchemaFormatterTest {
+
+  private static Properties props;
+  private static YangJsonSchemaMessageFormatter formatter;
+  private static YangSchema recordSchema = null;
+  private static String url = "mock://test";
+  private static ObjectMapper objectMapper = new ObjectMapper();
+  private static int idSize = 4;
+  private static SchemaRegistryClient schemaRegistry = null;
+
+  @BeforeAll
+  public static void setUp() {
+    props = new Properties();
+    props.put(KafkaYangJsonSchemaDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, url);
+    formatter = new YangJsonSchemaMessageFormatter();
+    formatter.init(props);
+    schemaRegistry =
+        new MockSchemaRegistryClient(Collections.singletonList(new YangSchemaProvider()));
+    YangSchemaContext schemaContext;
+    String schemaString;
+    try {
+      schemaContext =
+          YangYinParser.parse(
+              KafkaYangJsonSchemaSerializerTest.class
+                  .getClassLoader()
+                  .getResource("formatter/yangs/test.yang")
+                  .getFile());
+      schemaContext.validate();
+      schemaString = schemaContext.getModules().get(0).getOriginalString();
+    } catch (DocumentException | IOException | YangParserException e) {
+      throw new RuntimeException(e);
+    }
+    recordSchema =
+        (YangSchema)
+            schemaRegistry
+                .parseSchema(YangSchema.TYPE, schemaString, Collections.emptyList())
+                .get();
+  }
+
+  @AfterAll
+  public static void tearDown() {
+    MockSchemaRegistry.dropScope("test");
+  }
+
+  @Test
+  public void testKafkaYangJsonValueFormatter() throws JsonProcessingException {
+    String input = "{\"data\":{\"insa-test:insa-container\":{\"d\": \"test\"}}}";
+
+    BufferedReader reader =
+        new BufferedReader(new InputStreamReader(new ByteArrayInputStream(input.getBytes())));
+    YangJsonSchemaMessageReader yangJsonSchemaMessageReader =
+        new YangJsonSchemaMessageReader(
+            url, null, recordSchema, "topic1", false, reader, false, true, false);
+
+    ProducerRecord<byte[], byte[]> message = yangJsonSchemaMessageReader.readMessage();
+    byte[] serializedValue = message.value();
+
+    byte[] serializedSchemaId =
+        message.headers().lastHeader(AbstractKafkaYangJsonSchemaSerializer.SCHEMA_ID_KEY).value();
+    int schemaId = ByteBuffer.wrap(serializedSchemaId).getInt();
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(baos);
+
+    Headers headers = new RecordHeaders();
+    headers.add(
+        AbstractKafkaYangJsonSchemaSerializer.SCHEMA_ID_KEY,
+        ByteBuffer.allocate(idSize).putInt(schemaId).array());
+    ConsumerRecord<byte[], byte[]> crecord =
+        new ConsumerRecord<>(
+            "topic1",
+            0,
+            200,
+            1000,
+            TimestampType.LOG_APPEND_TIME,
+            0L,
+            0,
+            serializedValue.length,
+            null,
+            serializedValue,
+            headers);
+
+    formatter.writeTo(crecord, ps);
+
+    String output = baos.toString();
+
+    assertEquals(objectMapper.readTree(input), objectMapper.readTree(output));
+  }
+
+  @Test
+  public void testInvalidFormat() {
+    String input = "{\"data\":{\"insa-test:insa-container\":{\"d\": \"test\"";
+
+    BufferedReader reader =
+        new BufferedReader(new InputStreamReader(new ByteArrayInputStream(input.getBytes())));
+    YangJsonSchemaMessageReader yangJsonSchemaMessageReader =
+        new YangJsonSchemaMessageReader(
+            url, null, recordSchema, "topic1", false, reader, false, true, false);
+
+    assertThrowsExactly(SerializationException.class, yangJsonSchemaMessageReader::readMessage);
+  }
+}
