5d971122d85db638f28d027fe2f85932225a8f0c
Alex Huang Feng <alex.huang-feng@insa-lyon.fr>
2025-03-28 17:36:39 +0100

    Add YANG JSON serde

---
+++ yang-json-schema-serde/src/test/java/fr/insa/kafka/streams/serdes/yang/json/KafkaYangJsonSerdeTest.java
@@ -0,0 +1,102 @@
+package fr.insa.kafka.streams.serdes.yang.json;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import ch.swisscom.kafka.schemaregistry.yang.YangSchemaProvider;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import org.dom4j.DocumentException;
+import org.junit.jupiter.api.Test;
+import org.yangcentral.yangkit.common.api.validate.ValidatorResultBuilder;
+import org.yangcentral.yangkit.data.api.model.YangDataDocument;
+import org.yangcentral.yangkit.data.codec.json.YangDataDocumentJsonParser;
+import org.yangcentral.yangkit.model.api.schema.YangSchemaContext;
+import org.yangcentral.yangkit.parser.YangParserException;
+import org.yangcentral.yangkit.parser.YangYinParser;
+
+public class KafkaYangJsonSerdeTest {
+
+  private static final String ANY_TOPIC = "any-topic";
+
+  private static ObjectMapper objectMapper = new ObjectMapper();
+
+  private static KafkaYangJsonSerde<YangDataDocument> createConfiguredSerdeForRecordValues() {
+    SchemaRegistryClient schemaRegistryClient =
+        new MockSchemaRegistryClient(Collections.singletonList(new YangSchemaProvider()));
+    KafkaYangJsonSerde<YangDataDocument> serde = new KafkaYangJsonSerde<>(schemaRegistryClient);
+    Map<String, Object> serdeConfig = new HashMap<>();
+    serdeConfig.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "fake");
+    serde.configure(serdeConfig, false);
+    return serde;
+  }
+
+  private YangDataDocument getRecord() {
+    YangDataDocument doc;
+    String newString = "{\"data\":{\"insa-test:insa-container\":{\"d\":123}}}";
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      YangSchemaContext schemaContext =
+          YangYinParser.parse(this.getClass().getClassLoader().getResource("test.yang").getFile());
+      schemaContext.validate();
+      JsonNode jsonNode = mapper.readTree(newString);
+      doc =
+          new YangDataDocumentJsonParser(schemaContext)
+              .parse(jsonNode, new ValidatorResultBuilder());
+    } catch (DocumentException | IOException | YangParserException e) {
+      throw new RuntimeException(e);
+    }
+    return doc;
+  }
+
+  private JsonNode getJsonNode(YangDataDocument doc) {
+    JsonNode jsonNode;
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      jsonNode = mapper.readTree(doc.getDocString());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return jsonNode;
+  }
+
+  @Test
+  public void shouldRoundTripRecords() throws Exception {
+    KafkaYangJsonSerde<YangDataDocument> serde = createConfiguredSerdeForRecordValues();
+    YangDataDocument record = getRecord();
+
+    YangDataDocument roundTrippedRecord =
+        serde
+            .deserializer()
+            .deserialize(ANY_TOPIC, serde.serializer().serialize(ANY_TOPIC, record));
+
+    assertEquals(getJsonNode(record), getJsonNode(roundTrippedRecord));
+
+    serde.close();
+  }
+
+  @Test
+  public void shouldRoundTripNullRecordsToNull() {
+    KafkaYangJsonSerde<YangDataDocument> serde = createConfiguredSerdeForRecordValues();
+
+    YangDataDocument roundTrippedRecord =
+        serde.deserializer().deserialize(ANY_TOPIC, serde.serializer().serialize(ANY_TOPIC, null));
+
+    assertNull(roundTrippedRecord);
+
+    serde.close();
+  }
+
+  @Test
+  public void shouldFailWhenInstantiatedWithNullSchemaRegistryClient() {
+    assertThrowsExactly(
+        IllegalArgumentException.class,
+        () -> new KafkaYangJsonSerde<>((SchemaRegistryClient) null));
+  }
+}
---
+++ yang-json-schema-serializer/src/main/java/fr/insa/kafka/serializers/yang/json/KafkaYangJsonSchemaSerializerConfig.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2025 INSA Lyon.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.insa.kafka.serializers.yang.json;
+
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import java.util.Map;
+import org.apache.kafka.common.config.ConfigDef;
+
+public class KafkaYangJsonSchemaSerializerConfig extends AbstractKafkaSchemaSerDeConfig {
+
+  public static final String YANG_JSON_FAIL_UNKNOWN_PROPERTIES =
+      "yang.json.fail.unknown.properties";
+  public static final boolean YANG_JSON_FAIL_UNKNOWN_PROPERTIES_DEFAULT = true;
+  public static final String YANG_JSON_FAIL_UNKNOWN_PROPERTIES_DOC =
+      "Whether to fail serialization" + " if unknown YANG-JSON properties are encountered";
+  public static final String YANG_JSON_FAIL_INVALID_SCHEMA = "yang.json.fail.invalid.schema";
+  public static final boolean YANG_JSON_FAIL_INVALID_SCHEMA_DEFAULT = false;
+  public static final String YANG_JSON_FAIL_INVALID_SCHEMA_DOC =
+      "Whether to fail serialization if" + " the YANG-JSON payload does not match the schema";
+
+  private static ConfigDef config;
+
+  static {
+    config =
+        baseConfigDef()
+            .define(
+                YANG_JSON_FAIL_UNKNOWN_PROPERTIES,
+                ConfigDef.Type.BOOLEAN,
+                YANG_JSON_FAIL_UNKNOWN_PROPERTIES_DEFAULT,
+                ConfigDef.Importance.LOW,
+                YANG_JSON_FAIL_UNKNOWN_PROPERTIES_DOC)
+            .define(
+                YANG_JSON_FAIL_INVALID_SCHEMA,
+                ConfigDef.Type.BOOLEAN,
+                YANG_JSON_FAIL_INVALID_SCHEMA_DEFAULT,
+                ConfigDef.Importance.MEDIUM,
+                YANG_JSON_FAIL_INVALID_SCHEMA_DOC);
+  }
+
+  public KafkaYangJsonSchemaSerializerConfig(Map<?, ?> props) {
+    super(config, props);
+  }
+}
