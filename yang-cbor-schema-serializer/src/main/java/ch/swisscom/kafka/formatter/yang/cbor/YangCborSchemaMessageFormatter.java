c6c8126d0f3a48aad115f936ebc325055b2b2b3f
Ahmed Elhassany <Ahmed.Elhassany@swisscom.com>
2025-05-27 10:53:15 +0200

    Use ch.swisscom everywhere and update to v0.3

---
+++ yang-cbor-schema-serializer/src/main/java/ch/swisscom/kafka/formatter/yang/cbor/YangCborSchemaMessageFormatter.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2025 INSA Lyon.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.swisscom.kafka.formatter.yang.cbor;
+
+import ch.swisscom.kafka.schemaregistry.yang.YangSchemaProvider;
+import ch.swisscom.kafka.serializers.yang.cbor.AbstractKafkaYangCborSchemaDeserializer;
+import ch.swisscom.kafka.serializers.yang.cbor.KafkaYangCborSchemaDeserializerConfig;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.confluent.kafka.formatter.SchemaMessageDeserializer;
+import io.confluent.kafka.formatter.SchemaMessageFormatter;
+import io.confluent.kafka.schemaregistry.SchemaProvider;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.json.jackson.Jackson;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Map;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.yangcentral.yangkit.data.api.model.YangDataDocument;
+
+public class YangCborSchemaMessageFormatter extends SchemaMessageFormatter<YangDataDocument> {
+
+  private static final ObjectMapper objectMapper = Jackson.newObjectMapper();
+
+  public YangCborSchemaMessageFormatter() {}
+
+  @Override
+  protected SchemaMessageDeserializer createDeserializer(Deserializer keyDeserializer) {
+    return new YangCborSchemaMessageDeserializer(keyDeserializer);
+  }
+
+  @Override
+  protected void writeTo(String topic, Headers headers, byte[] data, PrintStream output)
+      throws IOException {
+    YangDataDocument object = deserializer.deserialize(topic, headers, data);
+    JsonNode jsonNode;
+    try {
+      jsonNode = objectMapper.readTree(object.getDocString());
+      output.println(objectMapper.writeValueAsString(jsonNode));
+    } catch (JsonProcessingException ignored) {
+      output.println(objectMapper.writeValueAsString("error reading value"));
+    }
+  }
+
+  @Override
+  protected SchemaProvider getProvider() {
+    return new YangSchemaProvider();
+  }
+
+  static class YangCborSchemaMessageDeserializer
+      extends AbstractKafkaYangCborSchemaDeserializer<JsonNode>
+      implements SchemaMessageDeserializer<YangDataDocument> {
+
+    protected final Deserializer keyDeserializer;
+
+    YangCborSchemaMessageDeserializer(Deserializer keyDeserializer) {
+      this.keyDeserializer = keyDeserializer;
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+      if (!configs.containsKey(
+          KafkaYangCborSchemaDeserializerConfig.YANG_CBOR_FAIL_INVALID_SCHEMA)) {
+        ((Map<String, Object>) configs)
+            .put(KafkaYangCborSchemaDeserializerConfig.YANG_CBOR_FAIL_INVALID_SCHEMA, "true");
+        configure(deserializerConfig(configs), null);
+      }
+    }
+
+    @Override
+    public Deserializer getKeyDeserializer() {
+      return keyDeserializer;
+    }
+
+    @Override
+    public Object deserializeKey(String topic, Headers headers, byte[] payload) {
+      return keyDeserializer.deserialize(topic, headers, payload);
+    }
+
+    @Override
+    public YangDataDocument deserialize(String topic, Headers headers, byte[] payload)
+        throws SerializationException {
+      return (YangDataDocument) super.deserialize(false, topic, isKey, headers, payload);
+    }
+
+    @Override
+    public SchemaRegistryClient getSchemaRegistryClient() {
+      return schemaRegistry;
+    }
+  }
+}
---
+++ yang-json-schema-serde/src/main/java/ch/swisscom/kafka/streams/serdes/yang/json/KafkaYangJsonSerde.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2025 INSA Lyon.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.swisscom.kafka.streams.serdes.yang.json;
+
+import ch.swisscom.kafka.serializers.yang.json.KafkaYangJsonSchemaDeserializer;
+import ch.swisscom.kafka.serializers.yang.json.KafkaYangJsonSchemaDeserializerConfig;
+import ch.swisscom.kafka.serializers.yang.json.KafkaYangJsonSchemaSerializer;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.Serializer;
+
+/**
+ * A schema-registry aware serde (serializer/deserializer) for Apache Kafka's Streams API that can
+ * be used for reading and writing data in YANG-JSON format.
+ */
+public class KafkaYangJsonSerde<T> implements Serde<T> {
+
+  private Class<T> specificClass;
+  private final Serde<T> inner;
+
+  public KafkaYangJsonSerde() {
+    inner =
+        Serdes.serdeFrom(
+            new KafkaYangJsonSchemaSerializer(), new KafkaYangJsonSchemaDeserializer());
+  }
+
+  public KafkaYangJsonSerde(Class<T> specificClass) {
+    this.specificClass = specificClass;
+    inner =
+        Serdes.serdeFrom(
+            new KafkaYangJsonSchemaSerializer(), new KafkaYangJsonSchemaDeserializer());
+  }
+
+  /** For testing purposes only. */
+  public KafkaYangJsonSerde(final SchemaRegistryClient client) {
+    this(client, null);
+  }
+
+  /** For testing purposes only. */
+  public KafkaYangJsonSerde(final SchemaRegistryClient client, final Class<T> specificClass) {
+    if (client == null) {
+      throw new IllegalArgumentException("schema registry client must not be null");
+    }
+    this.specificClass = specificClass;
+    inner =
+        (Serde<T>)
+            Serdes.serdeFrom(
+                new KafkaYangJsonSchemaSerializer<>(client),
+                new KafkaYangJsonSchemaDeserializer<>(client));
+  }
+
+  @Override
+  public Serializer<T> serializer() {
+    return inner.serializer();
+  }
+
+  @Override
+  public Deserializer<T> deserializer() {
+    return inner.deserializer();
+  }
+
+  @Override
+  public void configure(Map<String, ?> serdeConfig, final boolean isSerdeForRecordKeys) {
+    inner.serializer().configure(serdeConfig, isSerdeForRecordKeys);
+    inner
+        .deserializer()
+        .configure(withSpecificClass(serdeConfig, isSerdeForRecordKeys), isSerdeForRecordKeys);
+  }
+
+  @Override
+  public void close() {
+    inner.serializer().close();
+    inner.deserializer().close();
+  }
+
+  private Map<String, Object> withSpecificClass(final Map<String, ?> config, boolean isKey) {
+    if (specificClass == null) {
+      return (Map<String, Object>) config;
+    }
+    Map<String, Object> newConfig =
+        config == null ? new HashMap<String, Object>() : new HashMap<>(config);
+    if (isKey) {
+      newConfig.put(KafkaYangJsonSchemaDeserializerConfig.YANG_JSON_KEY_TYPE, specificClass);
+    } else {
+      newConfig.put(KafkaYangJsonSchemaDeserializerConfig.YANG_JSON_VALUE_TYPE, specificClass);
+    }
+    return newConfig;
+  }
+}
